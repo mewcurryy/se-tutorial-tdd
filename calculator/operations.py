d059e099521c34713d6e3ed3cbd2d28e6060581f
mercurred <davinpidada2005@gmail.com>
2025-05-06 16:30:49 +0700

    update the TDD files

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,17 +1,30 @@
-def add(a, b):
+def add(a : float, b : float) -> float:
+    """Return the sum of two number."""
     return a + b
 
 
-def subtract(a, b):
-    # TODO: implement this
-    pass
+def subtract(a : float, b : float) -> float:
+    """Return the subtraction of two number."""
+    return a - b
 
 
-def multiply(a, b):
-    # TODO: implement this
-    pass
+def multiply(a : float, b : float) -> float:
+    """Return the multiple result of two number."""
+    return a * b
 
 
-def divide(a, b):
-    # TODO: implement this (don't forget to handle division by zero)
-    pass
+def divide(a : float, b : float) -> float:
+    """Return the division result of two number."""
+    if b == 0:
+        print("Cannot divide by zero.")
+        pass
+    else:  
+        return a / b
+    
+    
+
+
+# def add(a : float, b : float) -> float:
+#     """Return the sum of two number."""
+#     # return a + b
+#     pass
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -6,18 +6,26 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(1, 2), 3)
 
     def test_subtract(self):
-        # TODO: add assertions for subtract
-        pass
+        self.assertEqual(subtract(3, 2), 1)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(7, 2), 5)
 
     def test_multiply(self):
-        # TODO: add assertions for multiply
-        pass
+        self.assertEqual(multiply(3, 2), 6)
+        self.assertEqual(multiply(5, 3.2), 16)
+        self.assertEqual(multiply(0, 2), 0)
+        self.assertEqual(multiply(7, 1), 7)
 
     def test_divide(self):
-        # TODO: add assertions for divide
-        pass
+        self.assertEqual(divide(3, 2), 1.5)
+        self.assertEqual(divide(6, 3), 2)
+        self.assertEqual(divide(0, 2), 0)
+        self.assertEqual(divide(7, 1), 7)
+        self.assertEqual(divide(7, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
